b787a621ba32cfb24aefedf4396cb75d35cce5cb
juice2227 <myceline2227@gmail.com>
2025-04-11 12:52:20 +0300

    kachiri and code hackathon using gemini AI

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">welcom ehome</h1>
+      <h2>welcome home</h2>
+    </>
+  );
+}
+
+export default App;
